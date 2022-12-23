210f462a74636b5b6582a7f37a4562da8fa5177a
Francisco jaque <fjaque330@gmail.com>
2022-12-23 09:40:16 -0300

    primero

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Boton from "./components/Boton.jsx";
+import Input from "./components/Input.jsx";
+
+
 
 function App() {
+  const [nombre, setNombre] = useState('');
+  const [password, setPassword] = useState('');
+
+  const BotonFormulario = () => {
+      if(password === '252525'){
+        return (<Boton></Boton>)
+      } else {
+        return (<></>)
+      }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Desafio de estados y componentes</h1>
+      <Input 
+          nombre={nombre}
+          setNombre={setNombre}
+          password={password}
+          setPassword={setPassword}
+          >
+      
+      </Input>
+       <BotonFormulario></BotonFormulario>
     </div>
   );
 }
